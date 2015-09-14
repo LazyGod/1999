9115095a082e2f026305a56c294bdf48b79e555d
Denis <laz.go@ya.ru>
2015-09-14 18:43:01 +0300

    Add
    0.0.1

---
+++ Assets/90-x/Scripts/Gun.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+
+public class Gun : MonoBehaviour {
+	
+	//Systeam
+	private float secondsBetweenShoots;
+	private float nextPossibleShootTime;
+	public float rpm;
+
+	//Components
+	public Transform spawn;
+	public Transform shellEjectionPoint;
+	private AudioSource audio;
+	private LineRenderer tracer;
+	public Rigidbody shell;
+	public enum GunType{Semi,Burst,Auto};
+	public GunType gunType;
+
+
+	void Start()
+	{
+		secondsBetweenShoots = 60 / rpm;
+		audio = GetComponent<AudioSource> ();
+
+		if (GetComponent<LineRenderer> ()) {
+			tracer = GetComponent<LineRenderer> ();
+		}
+	}
+
+	public void Shoot()
+	{
+
+		if (CanShoot ()) {
+			Ray ray = new Ray (spawn.position, spawn.forward);
+			RaycastHit Hit;
+
+			float shootDistance = 20;
+			if (Physics.Raycast (ray, out Hit, shootDistance))
+			{
+
+				shootDistance = Hit.distance;
+			}
+			nextPossibleShootTime = Time.time + secondsBetweenShoots;
+
+			audio.Play();
+
+			if(tracer)
+			{
+				StartCoroutine("RenderTracer", ray.direction * shootDistance);
+			}
+
+			Rigidbody newShell = Instantiate(shell,shellEjectionPoint.position,Quaternion.identity) as Rigidbody;
+			newShell.AddForce(shellEjectionPoint.forward * Random.Range(150f,200f) + spawn.forward * Random.Range (-10f,10f));
+	}
+	}
+
+	public void ShootContin()
+	{
+		if (gunType == GunType.Auto)
+		{
+			Shoot ();
+		}
+	}
+
+	private bool CanShoot()
+	{
+		bool canShoot = true;
+
+		if (Time.time < nextPossibleShootTime) 
+		{
+		canShoot = false;
+		}
+
+		return canShoot;
+	}
+	IEnumerator RenderTracer(Vector3 hitPoint)
+	{
+		tracer.enabled = true;
+		tracer.SetPosition(0,spawn.position);
+		tracer.SetPosition(1,spawn.position + hitPoint);
+
+		yield return null;
+		tracer.enabled = false;
+	}
+
+
+}
---
+++ Assets/90-x/Scripts/Shell.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shell : MonoBehaviour 
+{
+
+	private float lifeTime = 20;
+	private Material mat;
+	private Color originalColor;
+	private float fadePercent;
+	private float deathTime;
+	private bool fading;
+
+	void Start () 
+	{
+		mat = GetComponent<Renderer>().material;
+		originalColor = mat.color;
+
+		deathTime = Time.time + lifeTime;
+
+		StartCoroutine ("Fade");
+	}
+	
+IEnumerator Fade()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(.2f);
+			if (fading)
+			{
+				fadePercent += Time.deltaTime;
+				mat.color = Color.Lerp(originalColor,Color.clear,fadePercent);
+
+				if (fadePercent> 1)
+				{
+					Destroy(gameObject);
+				}
+			}
+			else
+			{
+				if (Time.time < deathTime)
+				{
+					fading = true;
+				}
+			}
+		}
+	}
+
+/*void OnTriggerEnter(Collider c)
+	{
+		if (c.tag == "Ground")
+		{
+			//GetComponent<Rigidbody>().Sleep();
+		}
+
+	}*/
+
+}
