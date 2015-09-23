9add25b861f1b45d32a9efaf3215f1681054ac0d
Denis <laz.go@ya.ru>
2015-09-23 17:52:18 +0300

    0.0.3
    +Car add

---
+++ Assets/90-x/Scripts/Spring.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour
+{
+
+
+    public Rigidbody targetObject;
+    public float spring;
+    public float minDistance = 0;
+    public float maxDistance = 0;
+
+    public float distance;
+
+
+    void Start()
+    {
+
+    }
+
+
+
+    void FixedUpdate()
+    {
+        distance = Mathf.Sqrt(Mathf.Pow(transform.position.x - targetObject.transform.position.x, 2) + Mathf.Pow(transform.position.y - targetObject.transform.position.y, 2) + Mathf.Pow(transform.position.z - targetObject.transform.position.z, 2));
+
+
+     /*   if (distance <= minDistance)
+        {
+            targetObject.AddExplosionForce((spring,( new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z)), distance);
+        }
+        */
+    }
+}
